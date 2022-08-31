2c18f7cb7c103408609298b50cbc4a42dbb8d456
Nathan Campos <nathan@innoveworkshop.com>
2022-08-31 18:10:28 +0100

    Tried a bit more of sanitization

--- bin/parse_index.pl
+++ bin/parse_index.pl
@@ -15,6 +15,8 @@ use utf8;
 use Carp;
 use Data::Dumper;
 
+our $fail_counter = 0;
+
 =head1 METHODS
 
 =over 4
@@ -37,15 +39,14 @@ sub main {
 	# Get the records in the index.
 	my @strrecords = slurp_records($ARGV[0]);
 
-	# Ignore the first record since it's just a header.
-	shift @strrecords;
-
 	# Go through the records parsing them.
 	my @records = ();
 	foreach my $rec (@strrecords) {
 		my %record = parse_record($rec);
 		push @records, \%record;
 	}
+
+	print "FAILS: $fail_counter\n";
 }
 
 =item I<%record> = C<parse_record>(I<$str>)
@@ -60,18 +61,22 @@ sub parse_record {
 
 	# Apply the big ugly regex.
 	$str =~ m/
-		^(?<exename>[^\s]+)\s+(?<url>[^\s]+)\s+(?<size>[^\s]+)\s+(?<date>[^\r\n]+)?[\r\n]{1,2}
-		TITLE:(?<title>[^\r\n]+)[\r\n]{1,2}
-		VERSION:(?<version>[^\r\n]+)[\r\n]{1,2}
-		LANGUAGE:(?<lang>[^\r\n]+)[\r\n]{1,2}
-		PRODUCTS\sAFFECTED:(?<products>[^\r\n]+)(?<ignored>[\r\n]{1,2}\s+\-[^\r\n]+)?[\r\n]{1,2}
-		OS:(?<os>[^\r\n]+)[\r\n]{1,2}
+		^(?<exename>[^\s]+)\s+(?<url>[^\s]+)\s+(?<size>[^\s]+)\s+(?<date>[^\r\n]+)?[\r\n]+
+		TITLE:(?<title>[^\r\n]+)[\r\n]+
+		VERSION:(?<version>[^\r\n]+)[\r\n]+
+		LANGUAGE:(?<lang>[^\r\n]+)[\r\n]+
+		PRODUCTS\sAFFECTED:(?<products>.+)(?=[\r\n]+OS:)[\r\n]+
+		OS:(?<os>.+)(?=[\r\n]+SUPERSEDES:)[\r\n]+
 		SUPERSEDES:(?<supersedes>[^\r\n]+)[\r\n]+
-	/x;
+	/sx;
 
 	# Check if we actually were able to parse the record.
 	if (not %+) {
-		croak "Couldn't parse the record";
+		print "FAILED TO PARSE THIS RECORD:\n";
+		print Dumper($str);
+		$fail_counter++;
+		carp "Couldn't parse the record";
+		print "\n\n";
 	}
 
 	# Capture groups in a writeable hash.
@@ -110,7 +115,38 @@ sub slurp_records {
 	local $/ = "\r\n\r\n";
 	my @records = <$fh>;
 	close $fh;
+
+	# Ignore the first record since it's just a header.
+	shift @records;
 	
+	return sanitize_records(@records);
+}
+
+=item I<@sanitized_recs> = C<sanitize_records>(I<@slurp_records>)
+
+Grabs the slurped records and sanitizes them a bit.
+
+=cut
+
+sub sanitize_records {
+	my @dirty = @_;
+	my @records = ();
+
+	# Go through the records finding the ones that were cut in half.
+	for (my $i = 0; $i < scalar(@dirty); $i++) {
+		my $record = $dirty[$i];
+		
+		# Check if the record is complete.
+		if ($record =~ /SUPERSEDES:([^\r\n]+)[\r\n]+$/) {
+			push @records, $record;
+		}
+
+		# Looks like we need to join two "records" that were split.
+		$i++;
+		$record .= $dirty[$i];
+		push @records, $record;
+	}
+
 	return @records;
 }
 
