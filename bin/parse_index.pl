4afc4a2c94f66a6156880643eedcbe095ad99ba4
Nathan Campos <nathan@innoveworkshop.com>
2022-08-31 13:26:38 +0100

    Created the template for the script that will parse the archive index

---
+++ bin/parse_index.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/env perl
+
+=head1 NAME
+
+C<App::SoftPAQIndexParser> - Parses the SoftPAQ archive index file.
+
+=cut
+
+package App::SoftPAQIndexParser;
+
+use strict;
+use warnings;
+use autodie;
+use utf8;
+
+=head1 METHODS
+
+=over 4
+
+=item C<main>()
+
+The script's main entry point.
+
+=cut
+
+sub main {
+	print "SoftPAQ Archive Index Parser\n";
+}
+
+main();
+
+__END__
+
+=back
+
+=head1 AUTHOR
+
+Nathan Campos <nathan@innoveworkshop.com>
+
+=head1 COPYRIGHT
+
+Copyright (c) 2022- Nathan Campos.
+
+=cut
